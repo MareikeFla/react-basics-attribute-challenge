2bf6bd054520e6bf9e72fe1f2f28c8ac85d54aed
MareikeFla <mwidderich@gmail.com>
2023-12-13 11:36:28 +0100

    add Article function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from "react";
 import "./styles.css";
 
+const Article = ({ uberschrift, platzhalterText }) => {
+  return (
+    <article>
+      <h2 className="article__title">{uberschrift}</h2>
+      <label htmlFor="inputText">Write text here: </label>
+      <input id="inputText" placeholder={platzhalterText}></input>
+      <a href="https://de.wikipedia.org/wiki/">Find info at Wikipedia</a>
+    </article>
+  );
+};
+
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return (
+    <Article
+      uberschrift={"Ich bin die Überschrift"}
+      platzhalterText={"Dynamischer Platzhalterin"}
+    />
+  );
 }
